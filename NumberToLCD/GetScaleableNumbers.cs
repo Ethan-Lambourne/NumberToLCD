070158a1317e15efd051449b273f3bb612cabb0a
Ethan Lambourne <100215369+Ethan-Lambourne@users.noreply.github.com>
2023-09-13 13:59:53 +0100

    Add files via upload

---
+++ NumberToLCD/GetScaleableNumbers.cs
@@ -0,0 +1,342 @@
+﻿namespace NumberToLCD
+{
+    public class GetScaleableNumbers
+    {
+        public string[] GetScaledNine(int numberHeight, int numberWidth)
+        {
+            List<string> nine = new();
+            for (int i = 0; i < numberHeight; i++)
+            {
+                for (int j = 0; j < numberWidth; j++)
+                {
+                    if (i == 0 || i == (numberHeight - 1) || i == (numberHeight / 2))
+                    {
+                        if (j == 0 || j == (numberWidth - 1))
+                        {
+                            nine.Add(" ");
+                        }
+                        else
+                        {
+                            nine.Add("_");
+                        }
+                    }
+                    else if (j == (numberWidth - 1) || (j == 0 && i < (numberHeight / 2)))
+                    {
+                        nine.Add("|");
+                    }
+                    else
+                    {
+                        nine.Add(" ");
+                    }
+                }
+                nine.Add("%");
+            }
+            string nineAsJoinedString = string.Join("", nine);
+            string[] ListOfNineElements = nineAsJoinedString.Split(new char[] { '%' });
+            return ListOfNineElements;
+        }
+
+        public string[] GetScaledEight(int numberHeight, int numberWidth)
+        {
+            List<string> eight = new();
+            for (int i = 0; i < numberHeight; i++)
+            {
+                for (int j = 0; j < numberWidth; j++)
+                {
+                    if (i == 0 || i == (numberHeight - 1) || i == (numberHeight / 2))
+                    {
+                        if (j == 0 || j == (numberWidth - 1))
+                        {
+                            eight.Add(" ");
+                        }
+                        else
+                        {
+                            eight.Add("_");
+                        }
+                    }
+                    else if (j == (numberWidth - 1) || j == 0)
+                    {
+                        eight.Add("|");
+                    }
+                    else
+                    {
+                        eight.Add(" ");
+                    }
+                }
+                eight.Add("%");
+            }
+            string eightAsJoinedString = string.Join("", eight);
+            string[] ListOfEightElements = eightAsJoinedString.Split(new char[] { '%' });
+            return ListOfEightElements;
+        }
+
+        public string[] GetScaledSeven(int numberHeight, int numberWidth)
+        {
+            List<string> seven = new();
+            for (int i = 0; i < numberHeight; i++)
+            {
+                for (int j = 0; j < numberWidth; j++)
+                {
+                    if (i == 0)
+                    {
+                        if (j == 0 || j == (numberWidth - 1))
+                        {
+                            seven.Add(" ");
+                        }
+                        else
+                        {
+                            seven.Add("_");
+                        }
+                    }
+                    else if (j == (numberWidth - 1))
+                    {
+                        seven.Add("|");
+                    }
+                    else
+                    {
+                        seven.Add(" ");
+                    }
+                }
+                seven.Add("%");
+            }
+            string sevenAsJoinedString = string.Join("", seven);
+            string[] ListOfSevenElements = sevenAsJoinedString.Split(new char[] { '%' });
+            return ListOfSevenElements;
+        }
+
+        public string[] GetScaledSix(int numberHeight, int numberWidth)
+        {
+            List<string> six = new();
+            for (int i = 0; i < numberHeight; i++)
+            {
+                for (int j = 0; j < numberWidth; j++)
+                {
+                    if (i == 0 || i == (numberHeight - 1) || i == (numberHeight / 2))
+                    {
+                        if (j == 0 || j == (numberWidth - 1))
+                        {
+                            six.Add(" ");
+                        }
+                        else
+                        {
+                            six.Add("_");
+                        }
+                    }
+                    else if (j == 0 || (j == (numberWidth - 1) && i > (numberHeight / 2)))
+                    {
+                        six.Add("|");
+                    }
+                    else
+                    {
+                        six.Add(" ");
+                    }
+                }
+                six.Add("%");
+            }
+            string sixAsJoinedString = string.Join("", six);
+            string[] ListOfSixElements = sixAsJoinedString.Split(new char[] { '%' });
+            return ListOfSixElements;
+        }
+
+        public string[] GetScaledFive(int numberHeight, int numberWidth)
+        {
+            List<string> five = new();
+            for (int i = 0; i < numberHeight; i++)
+            {
+                for (int j = 0; j < numberWidth; j++)
+                {
+                    if (i == 0 || i == (numberHeight - 1) || i == (numberHeight / 2))
+                    {
+                        if (j == 0 || j == (numberWidth - 1))
+                        {
+                            five.Add(" ");
+                        }
+                        else
+                        {
+                            five.Add("_");
+                        }
+                    }
+                    else if (j == 0 && i < (numberHeight / 2))
+                    {
+                        five.Add("|");
+                    }
+                    else if (j == (numberWidth - 1) && i > (numberHeight / 2))
+                    {
+                        five.Add("|");
+                    }
+                    else
+                    {
+                        five.Add(" ");
+                    }
+                }
+                five.Add("%");
+            }
+            string fiveAsJoinedString = string.Join("", five);
+            string[] ListOfFiveElements = fiveAsJoinedString.Split(new char[] { '%' });
+            return ListOfFiveElements;
+        }
+
+        public string[] GetScaledFour(int numberHeight, int numberWidth)
+        {
+            List<string> four = new();
+            for (int i = 0; i < numberHeight; i++)
+            {
+                for (int j = 0; j < numberWidth; j++)
+                {
+                    if (i == (numberHeight / 2))
+                    {
+                        if (j == 0 || j == (numberWidth - 1))
+                        {
+                            four.Add(" ");
+                        }
+                        else
+                        {
+                            four.Add("_");
+                        }
+                    }
+                    else if ((j == (numberWidth - 1) || (j == 0 && i < (numberHeight / 2))) && i != 0)
+                    {
+                        four.Add("|");
+                    }
+                    else
+                    {
+                        four.Add(" ");
+                    }
+                }
+                four.Add("%");
+            }
+            string fourAsJoinedString = string.Join("", four);
+            string[] ListOfFourElements = fourAsJoinedString.Split(new char[] { '%' });
+            return ListOfFourElements;
+        }
+
+        public string[] GetScaledThree(int numberHeight, int numberWidth)
+        {
+            List<string> three = new();
+            for (int i = 0; i < numberHeight; i++)
+            {
+                for (int j = 0; j < numberWidth; j++)
+                {
+                    if (i == 0 || i == (numberHeight - 1) || i == (numberHeight / 2))
+                    {
+                        if (j == 0 || j == (numberWidth - 1))
+                        {
+                            three.Add(" ");
+                        }
+                        else
+                        {
+                            three.Add("_");
+                        }
+                    }
+                    else if (j == (numberWidth - 1))
+                    {
+                        three.Add("|");
+                    }
+                    else
+                    {
+                        three.Add(" ");
+                    }
+                }
+                three.Add("%");
+            }
+            string threeAsJoinedString = string.Join("", three);
+            string[] ListOfThreeElements = threeAsJoinedString.Split(new char[] { '%' });
+            return ListOfThreeElements;
+        }
+
+        public string[] GetScaledTwo(int numberHeight, int numberWidth)
+        {
+            List<string> two = new();
+            for (int i = 0; i < numberHeight; i++)
+            {
+                for (int j = 0; j < numberWidth; j++)
+                {
+                    if (i == 0 || i == (numberHeight - 1) || i == (numberHeight / 2))
+                    {
+                        if (j == 0 || j == (numberWidth - 1))
+                        {
+                            two.Add(" ");
+                        }
+                        else
+                        {
+                            two.Add("_");
+                        }
+                    }
+                    else if (j == 0 && i > (numberHeight / 2))
+                    {
+                        two.Add("|");
+                    }
+                    else if (j == (numberWidth - 1) && i < (numberHeight / 2))
+                    {
+                        two.Add("|");
+                    }
+                    else
+                    {
+                        two.Add(" ");
+                    }
+                }
+                two.Add("%");
+            }
+            string twoAsJoinedString = string.Join("", two);
+            string[] ListOfTwoElements = twoAsJoinedString.Split(new char[] { '%' });
+            return ListOfTwoElements;
+        }
+
+        public string[] GetScaledOne(int numberHeight, int numberWidth)
+        {
+            List<string> one = new();
+            for (int i = 0; i < numberHeight; i++)
+            {
+                for (int j = 0; j < numberWidth; j++)
+                {
+                    if (j == (numberWidth / 2) && i != 0)
+                    {
+                        one.Add("|");
+                    }
+                    else
+                    {
+                        one.Add(" ");
+                    }
+                }
+                one.Add("%");
+            }
+            string oneAsJoinedString = string.Join("", one);
+            string[] ListOfOneElements = oneAsJoinedString.Split(new char[] { '%' });
+            return ListOfOneElements;
+        }
+
+        public string[] GetScaledZero(int numberHeight, int numberWidth)
+        {
+            List<string> zero = new();
+            for (int i = 0; i < numberHeight; i++)
+            {
+                for (int j = 0; j < numberWidth; j++)
+                {
+                    if (i == 0 || i == (numberHeight - 1))
+                    {
+                        if (j == 0 || j == (numberWidth - 1))
+                        {
+                            zero.Add(" ");
+                        }
+                        else
+                        {
+                            zero.Add("_");
+                        }
+                    }
+                    else if (j == 0 || j == (numberWidth - 1))
+                    {
+                        zero.Add("|");
+                    }
+                    else
+                    {
+                        zero.Add(" ");
+                    }
+                }
+                zero.Add("%");
+            }
+            string zeroAsJoinedString = string.Join("", zero);
+            string[] ListOfZeroElements = zeroAsJoinedString.Split(new char[] { '%' });
+            return ListOfZeroElements;
+        }
+    }
+}
---
+++ NumberToLCD/Program.cs
@@ -0,0 +1,185 @@
+﻿using NumberToLCD;
+
+Console.WriteLine("=============================================" +
+    "\nWelcome To My LCD Display Converter Doohicky!" +
+    "\n=============================================" +
+    "\n" +
+    "\nBefore we begin. please select which version of the program you would like to use:" +
+    "\n" +
+    "\n1. Neat numbers, but they're not scaleable." +
+    "\n2. Less-neat numbers, but you can choose how big they are.\n");
+
+int choice;
+if (int.TryParse(Console.ReadLine(), out choice))
+{
+    switch (choice)
+    {
+        case 1:
+            NeatLcdDisplay();
+            break;
+
+        case 2:
+            ScaleableLcdDisplay();
+            break;
+
+        default:
+            Console.WriteLine("Please enter a valid input (1 or 2).");
+            break;
+    }
+}
+else
+{
+    Console.WriteLine("\nPlease enter a valid integer.\n");
+}
+
+int GetIntWithInputValidation()
+{
+    int number;
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+        Console.WriteLine("\nPlease enter a valid integer.\n");
+    }
+    return number;
+}
+
+void ScaleableLcdDisplay()
+{
+    Console.WriteLine("\nEnter a number, and the program will show you that number as a scaleable LCD display!\n");
+    int numberToConvert = GetIntWithInputValidation();
+
+    Console.WriteLine("\nPlease enter the width of your number:   (WARNING: a width of 3 or below may not form a full number)\n");
+    int numberWidth = GetIntWithInputValidation();
+
+    Console.WriteLine("\nPlease enter the height of your number:  (WARNING: a height of 4 or below may not form a full number)\n");
+    int numberHeight = GetIntWithInputValidation();
+
+    var getScaleableNumbers = new GetScaleableNumbers();
+
+    string numberAsString = numberToConvert.ToString();
+
+    string[] zero = getScaleableNumbers.GetScaledZero(numberHeight, numberWidth);
+    string[] one = getScaleableNumbers.GetScaledOne(numberHeight, numberWidth);
+    string[] two = getScaleableNumbers.GetScaledTwo(numberHeight, numberWidth);
+    string[] three = getScaleableNumbers.GetScaledThree(numberHeight, numberWidth);
+    string[] four = getScaleableNumbers.GetScaledFour(numberHeight, numberWidth);
+    string[] five = getScaleableNumbers.GetScaledFive(numberHeight, numberWidth);
+    string[] six = getScaleableNumbers.GetScaledSix(numberHeight, numberWidth);
+    string[] seven = getScaleableNumbers.GetScaledSeven(numberHeight, numberWidth);
+    string[] eight = getScaleableNumbers.GetScaledEight(numberHeight, numberWidth);
+    string[] nine = getScaleableNumbers.GetScaledNine(numberHeight, numberWidth);
+
+    for (int y = 0; y < numberHeight; y++)
+    {
+        for (int x = 0; x < numberAsString.Length; x++)
+        {
+            if (numberAsString[x].Equals('0'))
+            {
+                Console.Write(zero[y]);
+            }
+            if (numberAsString[x].Equals('1'))
+            {
+                Console.Write(one[y]);
+            }
+            if (numberAsString[x].Equals('2'))
+            {
+                Console.Write(two[y]);
+            }
+            if (numberAsString[x].Equals('3'))
+            {
+                Console.Write(three[y]);
+            }
+            if (numberAsString[x].Equals('4'))
+            {
+                Console.Write(four[y]);
+            }
+            if (numberAsString[x].Equals('5'))
+            {
+                Console.Write(five[y]);
+            }
+            if (numberAsString[x].Equals('6'))
+            {
+                Console.Write(six[y]);
+            }
+            if (numberAsString[x].Equals('7'))
+            {
+                Console.Write(seven[y]);
+            }
+            if (numberAsString[x].Equals('8'))
+            {
+                Console.Write(eight[y]);
+            }
+            if (numberAsString[x].Equals('9'))
+            {
+                Console.Write(nine[y]);
+            }
+        }
+        Console.WriteLine("");
+    }
+}
+
+void NeatLcdDisplay()
+{
+    Console.WriteLine("\nEnter a number, and the program will show you that number as an LCD display!\n");
+
+    int number = GetIntWithInputValidation();
+
+    string[] zero = { " _ ", "| |", "|_|" };
+    string[] one = { "   ", " | ", " | " };
+    string[] two = { " _ ", " _|", "|_ " };
+    string[] three = { " _ ", " _|", " _|" };
+    string[] four = { "   ", "|_|", "  |" };
+    string[] five = { " _ ", "|_ ", " _|" };
+    string[] six = { " _ ", "|_ ", "|_|" };
+    string[] seven = { " _ ", "  |", "  |" };
+    string[] eight = { " _ ", "|_|", "|_|" };
+    string[] nine = { " _ ", "|_|", " _|" };
+
+    string numberAsString = number.ToString();
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < numberAsString.Length; j++)
+        {
+            if (numberAsString[j].Equals('0'))
+            {
+                Console.Write(zero[i]);
+            }
+            if (numberAsString[j].Equals('1'))
+            {
+                Console.Write(one[i]);
+            }
+            if (numberAsString[j].Equals('2'))
+            {
+                Console.Write(two[i]);
+            }
+            if (numberAsString[j].Equals('3'))
+            {
+                Console.Write(three[i]);
+            }
+            if (numberAsString[j].Equals('4'))
+            {
+                Console.Write(four[i]);
+            }
+            if (numberAsString[j].Equals('5'))
+            {
+                Console.Write(five[i]);
+            }
+            if (numberAsString[j].Equals('6'))
+            {
+                Console.Write(six[i]);
+            }
+            if (numberAsString[j].Equals('7'))
+            {
+                Console.Write(seven[i]);
+            }
+            if (numberAsString[j].Equals('8'))
+            {
+                Console.Write(eight[i]);
+            }
+            if (numberAsString[j].Equals('9'))
+            {
+                Console.Write(nine[i]);
+            }
+        }
+        Console.WriteLine("");
+    }
+}
